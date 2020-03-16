f12c4dc57e7048550d3c8541a636fd1641348473
a a <a@a>
2020-03-16 12:17:29 +0100

    printf

--- Fix.hs
+++ Fix.hs
@@ -2,6 +2,7 @@
 --  https://bartoszmilewski.com/2017/02/28/f-algebras/
 import Prelude
 import Data.Typeable
+import Text.Printf
 data Fix f = Fx (f (Fix f))
 unFix :: Fix f -> f (Fix f) 
 unFix (Fx f) = f
@@ -17,7 +18,15 @@ coalg (p : ns)    =  StreamF p ns
 alg   :: StreamF Int [Int]->[Int]
 alg NilF = []
 alg (StreamF e a)  = e:a
-showTypes f = print $ typeOf $ f
+showType
+  :: (PrintfArg t, Show a1, Typeable a2) => a1 -> t -> a2 -> String
+showType s n f = printf ("%"++(show s)++"s :: %s") n (show (typeOf f))
+showFunction :: Integer -> String -> IO ()
+showFunction l n = do
+    printf ("%"++(show l)++"s\n") "|"
+    printf ("%"++(show l)++"s\n") n
+    printf ("%"++(show l)++"s\n") "|"
+    printf ("%"++(show l)++"s\n") "V"
 main :: IO ()
 main      = do
     let a = [2..10]
@@ -25,12 +34,10 @@ main      = do
     print                                                                                a
     putStr          "                                                                    a ::          "
     print $ typeOf $                                                                     a
-    putStrLn        "                                                                                   |"
-    putStrLn        "                                                                                 coalg"
-    putStrLn        "                                                                                   |"
-    putStrLn        "                                                                                   v"
+    showFunction 85 "coalg"
     putStr          "                                                                coalg :: "
     print $ typeOf $                                                                 coalg
+    showFunction 85 "(fmap (ana coalg))"
     putStr          "                                           (fmap (ana coalg)) . coalg :: "
     print $ typeOf $                                            (fmap (ana coalg)) . coalg
     putStr          "                                      Fx . (fmap (ana coalg)) . coalg :: "
@@ -47,4 +54,3 @@ main      = do
     print $                                                   ((cata alg) . (ana coalg)) a
     putStr          "(alg . (fmap (cata alg)) . unFix . Fx . (fmap (ana coalg)) . coalg) a :: "
     print $          (alg . (fmap (cata alg)) . unFix . Fx . (fmap (ana coalg)) . coalg) a
-
