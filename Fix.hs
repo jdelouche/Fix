cae9dc90d7a3720340597a16d52671d0e59268b0
a a <a@a>
2020-03-18 16:32:28 +0100

    Fix.txt

--- Fix.hs
+++ Fix.hs
@@ -4,6 +4,7 @@ import Prelude
 import Data.Typeable
 import Text.Printf
 data Fix f = Fx (f (Fix f))
+data Fix2 f = Fx2 (f (Fix2 f))
 unFix :: Fix f -> f (Fix f) 
 unFix (Fx f) = f
 ana :: ([Int]->StreamF Int [Int]) -> ([Int] -> Fix (StreamF Int))
@@ -13,8 +14,8 @@ cata algF  = algF . (fmap (cata  algF)) . unFix
 data StreamF e a = NilF | StreamF e a deriving (Functor,Show)
 coalg :: [Int]->StreamF Int [Int]
 coalg []          =  NilF
-coalg (p : ns)    =  StreamF p ns
--- coalg (p : ns)    =  StreamF p (filter (notdiv p) ns) where notdiv p n = n `mod` p /= 0
+-- coalg (p : ns)    =  StreamF p ns
+coalg (p : ns)    =  StreamF p (filter (notdiv p) ns) where notdiv p n = n `mod` p /= 0
 alg   :: StreamF Int [Int]->[Int]
 alg NilF = []
 alg (StreamF e a)  = e:a
@@ -28,15 +29,20 @@ showFunction l n = do
     printf ("%"++(show (l+m))++"s\n") n
     printf ("%"++(show l)++"s\n") "|"
     printf ("%"++(show l)++"s\n") "V"
+showRes :: Integer -> String -> IO ()
+showRes l n = do
+    let m = truncate $ fromRational $ (toRational (length n)) / 2
+    printf ("%"++(show (l+m))++"s\n") n
 main :: IO ()
 main      = do
-    let a = [2..10]
+    let a = [2..15]
     let pad =  85
     putStr          "                                                                    a =  "
     print                                                                                a
     putStr          "                                                                    a ::          "
     print $ typeOf $                                                                     a
     showFunction pad                                                                "coalg"
+    showRes pad (show (coalg a))
     putStr          "                                                                coalg :: "
     print $ typeOf $                                                                 coalg
     showFunction pad                                           "(fmap (ana coalg))"
@@ -49,9 +55,11 @@ main      = do
     putStr          "                              unFix . Fx . (fmap (ana coalg)) . coalg :: "
     print $ typeOf $                               unFix . Fx . (fmap (ana coalg)) . coalg
     showFunction pad          "(fmap (cata alg))"
+    showRes pad  (show (((fmap (cata alg)) . unFix . Fx . (fmap (ana coalg)) . coalg) a))
     putStr          "          (fmap (cata alg)) . unFix . Fx . (fmap (ana coalg)) . coalg :: "
     print $ typeOf $           (fmap (cata alg)) . unFix . Fx . (fmap (ana coalg)) . coalg
     showFunction pad    "alg"
+    showRes pad  (show ((alg . (fmap (cata alg)) . unFix . Fx . (fmap (ana coalg)) . coalg) a))
     putStr          "    alg . (fmap (cata alg)) . unFix . Fx . (fmap (ana coalg)) . coalg :: "
     print $ typeOf $     alg . (fmap (cata alg)) . unFix . Fx . (fmap (ana coalg)) . coalg
     putStr          "                                             (cata alg) . (ana coalg) :: "
